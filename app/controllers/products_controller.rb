7ebdcb19fd3ded0cfab1c12cdb04bb560e555a53
Carol Greene <carolgreene3@gmail.com>
2018-06-23 16:09:16 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,12 @@
 class ProductsController < ApplicationController
 
-
   def index
     cart
   end
 
   def add
-
     cart << params[:product]
-    #binding.pry
+    binding.pry
     render :index
   end
 
